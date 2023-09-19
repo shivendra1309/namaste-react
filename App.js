58a3e176cef8b180c63574f09bca3de440332e9f
shivendra <shivendrapratapsinghchauhan.13@gmail.com>
2023-09-19 17:30:55 +0530

    added parcel

--- App.js
+++ App.js
@@ -1,35 +1,6 @@
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>hi shiv! This is h1</h1>
-        <div id="gc">
-            <h2>this is h2</h2>
-        </div> 
-    </div>
-    <div id="bro">
-        <p>This the the 1st p.</p>
-    </div> 
-</div> */
-}
+import React from "react";
+import  ReactDOM from "react-dom";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, [
-      "Hi Shiv!, This is H1",
-      React.createElement(
-        "div",
-        { id: "gc" },
-        React.createElement("h2", {}, "This is H2")
-      ),
-    ]),
-    React.createElement(
-      "div",
-      { id: "bro" },
-      React.createElement("p", {}, "This is the 1st P.")
-    )
-  ),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const heading = React.createElement("h1",{}, "hi shiv!");
+const root2 = ReactDOM.createRoot(document.getElementById("root1"));
+root2.render(heading);
