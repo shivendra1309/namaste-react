6d02e2e0300c3acf7d49946724d5d42004981edf
shivendra <shivendrapratapsinghchauhan.13@gmail.com>
2023-09-20 19:44:40 +0530

    component composition

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from "react";
-import  ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{}, "hi shiv!");
-const root2 = ReactDOM.createRoot(document.getElementById("root1"));
-root2.render(heading);
+const ReactElement = <h1>This is an element</h1>;
+const ReactComponent = () => <h1>This is an reactComponent</h1>;
+const ReactComponent1 = () => <h1>This is react component 1</h1>;
+const ReactComponent2 = () =>{ 
+  return(
+  <div className="container">
+    <ReactComponent />
+    <h1>This is reactComponent2</h1>
+    <ReactComponent1 />
+  </div>
+);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ReactComponent2 />);
